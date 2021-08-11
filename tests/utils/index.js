b449138512b5c23d51c70ed5d8efc343c2874f70
Thomas Roberts <thomas.roberts@automattic.com>
2021-08-11 15:49:08 +0100

    Add saveOrPublish test util

--- tests/utils/index.js
+++ tests/utils/index.js
@@ -4,4 +4,5 @@ export { visitBlockPage, visitPostOfType } from './visit-block-page';
 export { getBlockPagePermalink } from './get-block-page-permalink';
 export { getNormalPagePermalink } from './get-normal-page-permalink';
 export { clearAndFillInput } from './clear-and-fill-input';
+export { saveOrPublish } from './save-or-publish';
 export { shopper } from './shopper';
